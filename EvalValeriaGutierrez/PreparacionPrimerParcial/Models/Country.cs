6a64cf78324bab3198e38b341a4a0eb456752e4b
ValeriaGutierrez <ValeGe@DESKTOP-VESR4FH>
2021-03-13 11:38:06 -0400

    Se incorporo el modelo

---
+++ EvalValeriaGutierrez/PreparacionPrimerParcial/Models/Country.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PreparacionPrimerParcial.Models
+{
+    public class Country
+    {
+        [Key]
+        public string Name { get; set; }
+        [Required]
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public List<double> Latlng { get; set; }
+        public List<string> Timezones { get; set; }
+        public List<Currency> Currencies { get; set; }
+        public string Flag { get; set; }
+    }
+}
